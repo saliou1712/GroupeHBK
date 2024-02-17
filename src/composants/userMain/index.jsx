2d2086c4b6ed409f112fcb57c6b0798a59f472bb
salioudiop <salioudiop17122000@gmail.com>
2024-02-17 14:11:01 +0100

    v1.0.1

--- src/composants/userMain/index.jsx
+++ src/composants/userMain/index.jsx
@@ -1,5 +1,8 @@
+import { useNavigate } from "react-router-dom";
+
 function UserMain(){
     const username = localStorage.getItem("username")
+    const navigate = useNavigate()
 
     function formatString(chaine, end){
         if(chaine.length > end){
@@ -9,12 +12,18 @@ function UserMain(){
         return chaine.charAt(0).toUpperCase() + chaine.slice(1).toLowerCase();
     }
 
+    function Logout(){
+        localStorage.removeItem("token")
+        localStorage.removeItem("username")
+        navigate("/groupehbk/login")
+    }
+
     return(
         <div className="user_main">
             <div className="user_main__username">
                 <i class="fa-solid fa-user-tie"></i> {formatString(username, 15)}
             </div>
-            <div className="logout">
+            <div className="logout" onClick={()=>{Logout()}}>
                 <i class="fa-solid fa-arrow-right-from-bracket"></i> Déconnexion
             </div>
         </div>
